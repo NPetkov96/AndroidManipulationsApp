ae49e1dc26bf4c465c11657e25874b05f773c5a1
NPetkov96 <nikolaypetkow96@icloud.com>
2025-04-08 20:16:36 +0300

    coppy message from history

--- MedSestriManipulations/HistoryPage.xaml.cs
+++ MedSestriManipulations/HistoryPage.xaml.cs
@@ -8,6 +8,8 @@ public partial class HistoryPage : ContentPage
 {
     public ICommand ToggleCommand { get; }
     public ICommand RemoveCommand { get; }
+    public ICommand CopyCommand { get; }
+
 
     public HistoryPage()
     {
@@ -16,7 +18,26 @@ public partial class HistoryPage : ContentPage
         RemoveCommand = new Command<RequestHistoryEntry>(OnRemove);
         HistoryList.ItemsSource = HistoryService.HistoryItems;
         BindingContext = this;
+        CopyCommand = new Command<RequestHistoryEntry>(OnCopy);
+
     }
+
+    private async void OnCopy(RequestHistoryEntry entry)
+    {
+        string website = "www.medsestri.com";
+        string line = "-------------------------";
+        var total = entry.SelectedProcedures.Sum(p => p.Price);
+        var counter = 1;
+        var message = $"Пациент: {entry.Name}\nЕГН: {entry.EGN}\nТелефон: {entry.Phone}\n\n" +
+                              $"Избрани манипулации {entry.SelectedProcedures.Count}бр:\n" +
+                              string.Join("\n", entry.SelectedProcedures.Select(p => $"{counter++}.{p.Name} - {p.Price:F2} лв")) +
+                              $"\nУИН:{entry.UIN}" +
+                              $"\n\nОбщо сума: {total:F2} лв\n{line} \nСума с отстъпка: {(total * 0.8m):F2} лв" +
+                              $"\n{website}";
+
+        await Clipboard.SetTextAsync(message);
+    }
+
 
     private async void OnRemove(RequestHistoryEntry entry)
     {
